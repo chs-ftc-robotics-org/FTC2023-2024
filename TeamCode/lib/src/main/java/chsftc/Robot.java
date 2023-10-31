f87361b991e0bc06b6e6488668c98bb74d294c19
grekand46 <136272412+grekand46@users.noreply.github.com>
2023-10-31 12:40:12 -0400

    Delete TeamCode/lib/src directory

--- TeamCode/lib/src/main/java/chsftc/Robot.java
+++
@@ -1,8 +0,0 @@
-package chsftc;
-
-/* Final class that will bring together the other subclasses in this Java package
-   So we can access all the of the robot's functionality from one class */
-public class Robot {
-    // Leave this alone for now
-    public Drivetrain drivetrain = new Drivetrain();
-}
